93713eaa21cf0ca59f96d60c6b66991f502973d4
Teya <Teya@TVOROG>
2021-12-23 21:56:48 +0200

    Добавьте файлы проекта.

---
+++ 7.2 - string data type/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _7._2___string_data_type
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            string source = "Всем привет я dead inside.";
+
+            var replacement = source.Replace("Всем привет я", "***");
+            Console.WriteLine($"Исходная строка: <{source}>");
+            Console.WriteLine($"Обновленная строка: <{replacement}>");
+
+        }
+    }
+}
